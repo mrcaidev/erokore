d080ab3ef331597a225a9eb7953fa97f67233de0
Yuwang Cai <mrcaidev@gmail.com>
2025-09-11 10:16:28 +0800

    feat: 引入 shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from "react";
+import "./global.css";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <h1>Hello world!</h1>;
+  return <Button disabled>点我</Button>;
 }

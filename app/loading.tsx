6dda770f429472e5a8e586e14f1ed129a261da1e
Yuwang Cai <mrcaidev@gmail.com>
2025-09-11 17:34:04 +0800

    feat: global loading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2Icon } from "lucide-react";
+
+const RootLoading = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+      <Loader2Icon className="size-12 animate-spin" />
+    </div>
+  );
+};
+
+export default RootLoading;

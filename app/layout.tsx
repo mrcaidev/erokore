ec6f91472e4b271d0faea10ac4f564881d9ee119
Yuwang Cai <mrcaidev@gmail.com>
2025-09-11 17:47:05 +0800

    feat: noto sans sc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from "react";
 import "./global.css";
+import "@fontsource/noto-sans-sc";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 

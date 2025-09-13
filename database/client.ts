32c54a49709fc1b693bdeaa037f3c40b8c1b22b3
Yuwang Cai <mrcaidev@gmail.com>
2025-09-13 11:58:19 +0800

    feat: use node-postgres in development

--- database/client.ts
+++ database/client.ts
@@ -1,4 +1,10 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.POSTGRES_URL ?? "", { schema });
+const POSTGRES_URL = process.env.POSTGRES_URL ?? "";
+
+export const db =
+  process.env.NODE_ENV === "production"
+    ? drizzleNeon(POSTGRES_URL, { schema })
+    : drizzlePg(POSTGRES_URL, { schema });

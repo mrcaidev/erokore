958db19c0b0250675830df00b4c0086d9c810c80
Yuwang Cai <mrcaidev@gmail.com>
2025-09-21 23:36:30 +0800

    feat: homepage

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,5 +1,26 @@
+import { FolderPlusIcon, ListIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <main className="grid place-items-center h-screen">Home</main>;
+  return (
+    <main className="grid place-items-center h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+        <Button variant="secondary" asChild>
+          <Link href="/subscriptions">
+            <ListIcon />
+            我关注的
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/collections/create">
+            <FolderPlusIcon />
+            创建作品集
+          </Link>
+        </Button>
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
---
+++ app/(app)/subscriptions/page.tsx
@@ -0,0 +1,7 @@
+import { UnderConstructionScreen } from "@/components/under-construction-screen";
+
+const SubscriptionsPage = () => {
+  return <UnderConstructionScreen />;
+};
+
+export default SubscriptionsPage;
